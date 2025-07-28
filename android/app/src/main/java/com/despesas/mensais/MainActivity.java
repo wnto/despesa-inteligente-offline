a273f0af0a1a76380ccb09cf9ceb7c366a8753ce
nexus <ferbress@gmail.com>
2025-07-28 15:15:05 -0300

    Progress on plugin setup and echo test

---
+++ android/app/src/main/java/com/despesas/mensais/MainActivity.java
@@ -0,0 +1,5 @@
+package com.despesas.mensais;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.despesas.mensais',
+  appName: 'Despesas Mensais',
+  webDir: 'dist'
+};
+
+export default config;
